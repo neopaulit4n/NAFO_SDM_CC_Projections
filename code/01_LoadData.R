23e65ccf9c513e95eb6e2a39a9d90ad9240b9ed1
neopaulit4n <159934381+neopaulit4n@users.noreply.github.com>
2026-01-06 17:05:20 -0400

    Preliminary RF modelling
    Extracted environmental predictor values (from raster) at sampling locations (lat/lon values). Combined predictor and response values into combined dataframe. Ran preliminary RF models across VME groups.
    Exploratory plots: instead of one plot showing all VME groups PA, made one plot per VME and made plot grid.

--- code/01_LoadData.R
+++ code/01_LoadData.R
@@ -49,7 +49,37 @@ names(bnam_layers) <- sapply(names(bnam_layers), match_cmip_names)
 bnam_layers <- bnam_layers[!is.na(names(bnam_layers))]
 
 
+# Extract predictor values at response locations ----
+pred_values <- lapply(bnam_layers, function(layer) {
+  terra::extract(layer, 
+                 select(resp_df, Start_Long_DD, Start_Lat_DD)) %>%
+    select(-ID)
+}) %>%
+  bind_cols()
+colnames(pred_values) <- names(bnam_layers)
 
+# Combine predictor and response dataframes ----
+comb_df <- bind_cols(resp_df, pred_values) %>%
+  mutate(VME_P_A = as.factor(VME_P_A))
 
+# Remove NA
+comb_df_compl <- comb_df %>%
+  drop_na()
 
+comb_df_miss <- comb_df[which(!complete.cases(comb_df)),]
 
+# Preliminary RF model test ----
+rf_test_form <- as.formula(paste("VME_P_A ~", 
+                                 paste(colnames(pred_values), collapse = " + ")))
+rf_test <- randomForest::randomForest(formula = rf_test_form,
+                        data = filter(comb_df_compl, VME_Group == "boltenia"),
+                        importance=TRUE)
+rf_test <- lapply(unique(comb_df_compl$VME_Group), function(vme_group) {
+  rf_model <- randomForest::randomForest(formula = rf_test_form,
+                                        data = filter(comb_df_compl, VME_Group == vme_group),
+                                        importance=TRUE)
+  return(rf_model)
+}) %>%
+  set_names(unique(comb_df_compl$VME_Group))
+
+rf_test
--- code/02_ExploratoryPlots.R
+++ code/02_ExploratoryPlots.R
@@ -20,22 +20,31 @@ resp_sf <- sf::st_as_sf(resp_df, coords = c("Start_Long_DD", "Start_Lat_DD"), cr
 
 ##
 # Map the NAFO boundary and plot response data points ----
-boundary_plot <- ggplot() +
-  theme_classic() +
-  geom_sf(data = sa, aes(colour = "NAFO Study Area"), fill = "lightblue", alpha = 0.8, show.legend = "line") +
-  # Adjust colours
-  scale_colour_manual(name = "Boundary", 
-                      values = c("NAFO Study Area" = "black")) +
-  geom_contour(data = bathy_noaa, 
-               aes(x = x, y = y, z = z, fill = NULL), 
-               breaks = seq(from = -50, to = -5000, by = -100),
-               color = "darkgrey", 
-               linewidth = 0.3, 
-               alpha = 0.4) +
-  # Add response points
-  geom_sf(data = resp_sf, aes(fill = as.factor(VME_P_A)), 
-          shape = 21, size = 2, alpha = 0.3) +
-  labs(x = "Longitude", y = "Latitude") +
-  scale_x_continuous(expand = c(0, 0)) +
-  scale_y_continuous(expand = c(0, 0))
-boundary_plot
+
+plotlist_vme_pa <- lapply(unique(resp_sf$VME_Group), function(vme_group) {
+  p <- ggplot() +
+    theme_classic() +
+    geom_sf(data = sa, aes(colour = "NAFO Study Area"), fill = "lightblue", alpha = 0.8, colour = "black") +
+    # # Adjust colours
+    # scale_colour_manual(name = "Boundary", 
+    #                     values = c("NAFO Study Area" = "black")) +
+    geom_contour(data = bathy_noaa, 
+                 aes(x = x, y = y, z = z, fill = NULL), 
+                 breaks = seq(from = -50, to = -5000, by = -100),
+                 color = "darkgrey", 
+                 linewidth = 0.3, 
+                 alpha = 0.4) +
+    # Add response points
+    geom_sf(data = resp_sf %>% filter(VME_Group == vme_group), 
+            aes(fill = as.factor(VME_P_A)), 
+            shape = 21, size = 2, alpha = 0.3) +
+    labs(title = vme_group,
+         x = "Longitude", y = "Latitude") +
+    scale_x_continuous(expand = c(0, 0)) +
+    scale_y_continuous(expand = c(0, 0)) +
+    # Remove legend
+    theme(legend.position = "none")
+})
+
+cowplot::plot_grid(plotlist = plotlist_vme_pa, ncol = 4)
+
