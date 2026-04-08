31ed6a1da68f6cbdec69b341968f30124f14c36a
neopaulit4n <159934381+neopaulit4n@users.noreply.github.com>
2026-04-08 09:46:07 -0300

    Update project structure 03_LoopThroughParams now in main code and renamed 00_CodeCaller. 03_SelectTopVars moved to DataCleanup folder. 05_VarCorrTimeSeries moved to Exploratory.

---
+++ code/02_Exploratory/05_VarCorrTimeSeries.R
@@ -0,0 +1,8 @@
+
+# Exploring how variable correlations change across time periods (stationarity of variable correlations)
+
+# Ellen suggested looking into Augmented Dickey-Fuller (ADF) or Wald tests but not sure if they're suitable for testing variable correlations changes?
+# Examples I see for ADF are for only one variable time series, not correlations between variables
+
+# I want to plot correlations between variable pairs over time
+
