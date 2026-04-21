0dd4a26c67e852a1a5b063bb5f54ac1d2ddb195e
neopaulit4n <159934381+neopaulit4n@users.noreply.github.com>
2026-04-21 17:13:53 -0300

    Update to variable correlation plots Outputs variable correlation plots for all 44 CMIP variables and black coral's CHNETBL3 terrain variable across all periods and SSPs.

--- code/02_Exploratory/05_VarCorrTimeSeries.R
+++ code/02_Exploratory/05_VarCorrTimeSeries.R
@@ -67,17 +67,40 @@ all_layers_cor <- lapply(1:length(all_layers_df), function(layer) {
   # cat(names(layer))
   cor_mat <- cor(all_layers_df[[layer]], method = "spearman", use = "complete.obs") |>
     as.data.frame()
-  # cor_long <- as.data.frame(cor_mat) %>%
-  #   rownames_to_column(var = "var1") %>%
-  #   pivot_longer(-var1, names_to = "var2", values_to = "cor")
-  # return(cor_long)
-  write.csv(cor_mat, paste0("output/01_Exploratory/04_CorrelationMatrices/correlation_matrices/",names(all_layers_df)[layer],"_cor.csv"))
-}) #%>%
-  # bind_rows(.id = "PeriodSSP") %>%
-  # mutate(
-  #   period = ifelse(is.na(str_extract(PeriodSSP, "P\\d")), "baseline", str_extract(PeriodSSP, "P\\d")),
-  #   ssp = ifelse(is.na(str_extract(PeriodSSP, "\\d-\\d\\.\\d")), "", str_extract(PeriodSSP, "\\d-\\d\\.\\d"))
-  # )
+  cor_long <- as.data.frame(cor_mat) %>%
+    rownames_to_column(var = "var1") %>%
+    pivot_longer(-var1, names_to = "var2", values_to = "cor")
+  return(cor_long)
+  # write.csv(cor_mat, paste0("output/01_Exploratory/04_CorrelationMatrices/correlation_matrices/",names(all_layers_df)[layer],"_cor.csv"))
+}) %>%
+  set_names(names(all_layers_df)) %>%
+  bind_rows(.id = "PeriodSSP") %>%
+  mutate(
+    period = ifelse(is.na(str_extract(PeriodSSP, "P\\d")), "baseline", str_extract(PeriodSSP, "P\\d")),
+    ssp = ifelse(is.na(str_extract(PeriodSSP, "\\d-\\d\\.\\d")), "", str_extract(PeriodSSP, "\\d-\\d\\.\\d"))
+  )
+
+lapply(names(all_layers_df), function(x) {
+  df <- filter(all_layers_cor, PeriodSSP == x)
+  x_name <- ifelse(x == "baseline", "Reference", paste(df$period[1], "SSP", df$ssp[1]))
+  p <- ggplot(data = df, aes(x = var1, y = var2, fill = cor)) +
+    geom_tile(colour = "black") +
+    geom_text(aes(label = ifelse(cor > 0.7 | cor < -0.7, "*", ""))) +
+    scale_fill_gradient2(low = "blue", mid = "white", high = "red", limit = c(-1,1), midpoint = 0) +
+    scale_x_discrete(expand = c(0,0)) +
+    scale_y_discrete(expand = c(0,0)) +
+    theme_classic() +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1),
+          axis.text = element_text(size = 8),
+          axis.line = element_blank(),
+          axis.title = element_blank()) +
+    labs(fill = "Correlation",
+        title = x_name)
+  ggsave(
+    filename = paste0(output_folder,"/",vmeoi,"_plot_cor_AllCMIPVars_FullArea_",x_name,".jpg"),
+    plot = p, width = 8, height = 6
+  )
+})
 
 
 # Tidy dataframe
@@ -87,56 +110,56 @@ all_layers_cor <- lapply(1:length(all_layers_df), function(layer) {
 #   pivot_longer(-var1, names_to = "var2", values_to = "cor") #%>%
 #   # filter(var1 != var2)
 
-p1 <- ggplot(data = filter(vme_layers_cor, period == "baseline"), aes(x = var1, y = var2, fill = cor)) +
-  geom_tile(colour = "black") +
-  # geom_text(aes(label = ifelse(cor > 0.7 | cor < -0.7, "*", ""))) +
-  scale_fill_gradient2(low = "blue", mid = "white", high = "red", limit = c(-1,1), midpoint = 0) +
-  scale_x_discrete(expand = c(0,0)) +
-  scale_y_discrete(expand = c(0,0)) +
-  theme_classic() +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1),
-        #axis.text = element_text(size = 6),
-        axis.line = element_blank(),
-        axis.title = element_blank()) +
-  labs(fill = "Correlation",
-       title = "Baseline")
-ggsave(paste0(main_output_folder,"cor_plot_baseline.jpg"), plot = p1)
+# p1 <- ggplot(data = filter(vme_layers_cor, period == "baseline"), aes(x = var1, y = var2, fill = cor)) +
+#   geom_tile(colour = "black") +
+#   # geom_text(aes(label = ifelse(cor > 0.7 | cor < -0.7, "*", ""))) +
+#   scale_fill_gradient2(low = "blue", mid = "white", high = "red", limit = c(-1,1), midpoint = 0) +
+#   scale_x_discrete(expand = c(0,0)) +
+#   scale_y_discrete(expand = c(0,0)) +
+#   theme_classic() +
+#   theme(axis.text.x = element_text(angle = 45, hjust = 1),
+#         #axis.text = element_text(size = 6),
+#         axis.line = element_blank(),
+#         axis.title = element_blank()) +
+#   labs(fill = "Correlation",
+#        title = "Baseline")
+# ggsave(paste0(main_output_folder,"cor_plot_baseline.jpg"), plot = p1)
 
-p2 <- ggplot(data = filter(vme_layers_cor, period != "baseline"), aes(x = var1, y = var2, fill = cor)) +
-  facet_wrap(~ssp + period) +
-  geom_tile(colour = "black") +
-  # geom_text(aes(label = ifelse(cor > 0.7 | cor < -0.7, "*", ""))) +
-  scale_fill_gradient2(low = "blue", mid = "white", high = "red", limit = c(-1,1), midpoint = 0) +
-  scale_x_discrete(expand = c(0,0)) +
-  scale_y_discrete(expand = c(0,0)) +
-  theme_classic() +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1),
-        #axis.text = element_text(size = 6),
-        axis.line = element_blank(),
-        axis.title = element_blank()) +
-  labs(fill = "Correlation")
-ggsave(paste0(main_output_folder,"cor_plot_periodssp.jpg"), plot = p2)
+# p2 <- ggplot(data = filter(vme_layers_cor, period != "baseline"), aes(x = var1, y = var2, fill = cor)) +
+#   facet_wrap(~ssp + period) +
+#   geom_tile(colour = "black") +
+#   # geom_text(aes(label = ifelse(cor > 0.7 | cor < -0.7, "*", ""))) +
+#   scale_fill_gradient2(low = "blue", mid = "white", high = "red", limit = c(-1,1), midpoint = 0) +
+#   scale_x_discrete(expand = c(0,0)) +
+#   scale_y_discrete(expand = c(0,0)) +
+#   theme_classic() +
+#   theme(axis.text.x = element_text(angle = 45, hjust = 1),
+#         #axis.text = element_text(size = 6),
+#         axis.line = element_blank(),
+#         axis.title = element_blank()) +
+#   labs(fill = "Correlation")
+# ggsave(paste0(main_output_folder,"cor_plot_periodssp.jpg"), plot = p2)
 
 # Testing equality of correlation matrices
 
 # Use tool eqCorrMatTest from ldstatsHD packages (cran/ldstatsHD)
 # ldstatsHD relies on camel dependency (cran/camel)
 
-ldstatsHD::eqCorrMatTest()
+# ldstatsHD::eqCorrMatTest()
 
 
-EX2 <- ldstatsHD::pcorSimulatorJoint(nobs = 50, nclusters = 3, nnodesxcluster = c(40,40,40), 
-                          pattern = "pow", diffType = "cluster", dataDepend = "diag", 
-                          pdiff=0.5)	
+# EX2 <- ldstatsHD::pcorSimulatorJoint(nobs = 50, nclusters = 3, nnodesxcluster = c(40,40,40), 
+#                           pattern = "pow", diffType = "cluster", dataDepend = "diag", 
+#                           pdiff=0.5)	
 
-d1 <- as.matrix(vme_pred_df$`P1.1-2.6`)
-d2 <- as.matrix(vme_pred_df$`P1.2-4.5`)
+# d1 <- as.matrix(vme_pred_df$`P1.1-2.6`)
+# d2 <- as.matrix(vme_pred_df$`P1.2-4.5`)
 
-test1 <- ldstatsHD::eqCorrMatTest(
-  d1, 
-  d2, 
-  testStatistic = c("AS","max","exc"), 
-  excAdj = FALSE,
-  # testNullDist = "asyDep",
-  paired = TRUE, 
-  nite = 400)
+# test1 <- ldstatsHD::eqCorrMatTest(
+#   d1, 
+#   d2, 
+#   testStatistic = c("AS","max","exc"), 
+#   excAdj = FALSE,
+#   # testNullDist = "asyDep",
+#   paired = TRUE, 
+#   nite = 400)
