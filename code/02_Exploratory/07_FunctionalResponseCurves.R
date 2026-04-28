1e36e8a368a6323daeb7df02c9a4ed03ca50cd1e
neopaulit4n <159934381+neopaulit4n@users.noreply.github.com>
2026-04-28 17:10:28 -0300

    Output functional response curve with points coloured by NAFO divisions

--- code/02_Exploratory/07_FunctionalResponseCurves.R
+++ code/02_Exploratory/07_FunctionalResponseCurves.R
@@ -5,11 +5,35 @@
 # Extract environmental values
 # Plot relationship using loess smoother
 
+# Read in raster layers ----
+rf_res_presprob_baseline <- terra::rast(paste0(output_folder,"/rasters/",vmeoi,"_rf_res_baseline_rawPresenceProb.tif"))
+
+rf_res_presprob_proj <- lapply(
+  list.files(
+    path = paste0(output_folder,"/rasters"),
+    pattern = "rf_res_proj_rawPresenceProb",
+    full.names = TRUE),
+  function(x) {
+    terra::rast(x)
+}) |>
+  set_names(str_extract(
+    list.files(path = paste0(output_folder,"/rasters"), pattern = "rf_res_proj_rawPresenceProb"),
+    pattern = "P\\d_\\d-\\d\\.\\d"
+  ))
+
+# Load NAFO division areas ----
+nafo_div <- sf::st_read("data/raw/Mapping_Layers/NAFO_Divisions/NAFO_Divisions_SHP/NAFO_Divisions_2021_poly_not_clipped.shp") |>
+  sf::st_transform(4326) |>
+  select(Division = Label, geometry)
+
 frc_baseline <- bind_cols(
   terra::as.data.frame(vme_layers_baseline),
-  terra::as.data.frame(rf_res_presprob_baseline)
-) %>%
-  pivot_longer(-mean, names_to = "Variable", values_to = "Value")
+  terra::as.data.frame(rf_res_presprob_baseline, xy = TRUE)
+) |>
+  # pivot_longer(-c(mean, x, y), names_to = "Variable", values_to = "Value") #|>
+  # Join NAFO divisions labels to points
+  sf::st_as_sf(coords = c("x","y"), crs = 4326) |>
+  sf::st_join(nafo_div)
 
 ggplot(data = frc_baseline, aes(x = Value, y = mean_pres_prob)) +
   facet_wrap(~ Variable, scales = "free") +
@@ -52,10 +76,13 @@ frc_df <- lapply(1:4, function(sspoi) {
       drop_na() |>
       bind_cols(
         rf_res_presprob_proj[[grep(
-          paste0(period_all[poi],"\\.",ssp_all[sspoi]), 
+          paste0(period_all[poi],"_",ssp_all[sspoi]), 
           names(rf_res_presprob_proj))
         ]] |>
-          terra::as.data.frame()
+          terra::as.data.frame(xy = TRUE) |>
+          # Join NAFO division labels to points
+          sf::st_as_sf(coords = c("x","y"), crs = 4326) |>
+          sf::st_join(nafo_div)
       )
   })
   pred_df[[5]] <- frc_baseline
@@ -65,12 +92,13 @@ frc_df <- lapply(1:4, function(sspoi) {
 }) |>
   set_names(ssp_all) |>
   bind_rows(.id = "SSP") |>
-  pivot_longer(-c(mean, SSP, Period), names_to = "Variable", values_to = "Value")
+  select(-geometry) |>
+  pivot_longer(-c(mean, SSP, Period, Division), names_to = "Variable", values_to = "Value")
 
-ggplot(z, aes(x = Value, y = mean, colour = Period)) +
-  # facet_wrap(~Variable, scales = "free") +
+ggplot(frc_df, aes(x = Value, y = mean)) +
   facet_grid(vars(SSP), vars(Variable), scales = "free") +
-  geom_smooth(alpha = 0.7) +  # uses GAM by default
+  geom_point(aes(colour = Division), alpha = 0.2) +
+  geom_smooth(alpha = 0.7, colour = "black") +  # uses GAM by default
   # stat_summary(
   #   fun.data = function(x) {
   #     data.frame(
@@ -100,4 +128,23 @@ ggplot(z, aes(x = Value, y = mean, colour = Period)) +
   labs(y = "Mean predicted presence probability")
 
 ggsave(paste0(output_folder,"/",vmeoi,"_FunctionalResponseCurveGAM.jpg"),
+  width = 12, height = 8, dpi = 500)
+
+# Plots with points coloured by NAFO division ----
+z <- filter(frc_df, Period == "Reference", SSP == "1-2.6")
+ggplot(z, aes(x = Value, y = mean)) +
+  facet_wrap(~ Variable, scales = "free") +
+  geom_point(aes(colour = Division), alpha = 0.2) +
+  geom_smooth(alpha = 0.7, colour = "black") +  # uses GAM by default
+  # stat_summary(
+  #   fun.data = function(x) {
+  #     data.frame(
+  #       y = mean(x, na.rm = TRUE),
+  #       ymin = mean(x, na.rm = TRUE) - 1.96 * sd(x, na.rm = TRUE)/sqrt(length(x[!is.na(x)])),
+  #       ymax = mean(x, na.rm = TRUE) + 1.96 * sd(x, na.rm = TRUE)/sqrt(length(x[!is.na(x)]))
+  #     )},
+  #     geom = "ribbon", alpha = 0.2, colour = NA) +
+  theme_bw() +
+  labs(y = "Mean predicted presence probability", title = "Reference")
+ggsave(paste0(output_folder,"/",vmeoi,"_FunctionalResponseCurveGAM_ReferencePts.jpg"),
   width = 12, height = 8, dpi = 500)
