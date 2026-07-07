f2425d8eb4c9cb0d228b966059e53a098e16bb97
neopaulit4n <159934381+neopaulit4n@users.noreply.github.com>
2026-07-07 10:11:17 -0300

    Create combined plot of baseline MaxClass, MaxClassF, and MaxClassAvgProb Using `patchwork` to copy final formatted figure for paper.

--- code/04_Mapping.R
+++ code/04_Mapping.R
@@ -197,6 +197,16 @@ rf_pred_maps <- lapply(metric_names, function(metric) {
 
   ggsave(paste0(output_folder,"/ModellingMaps/",vmeoi,"_baseline_",metric,".jpg"), p_baseline,
     width = 10, height = 10, dpi = 300)
-})
-
   
+  return(list(p, p_baseline))
+})
+names(rf_pred_maps) <- metric_names
+
+# Create combined plot of baseline thresholded PA (A), MaxClassF (B), MaxClassAvgProb (C) ----
+library(patchwork)
+rf_pred_maps$MaxClass[[2]] + ggtitle("A") + 
+  rf_pred_maps$MaxClassF[[2]] + ggtitle("B") +
+  rf_pred_maps$MaxClassAvgProb[[2]] + ggtitle("C") +
+  patchwork::plot_layout(nrow = 1)
+ggsave(paste0(output_folder,"/ModellingMaps/",vmeoi,"_baseline_threeplotcombined.jpg"),
+  width = 10, height = 3, dpi = 300)
